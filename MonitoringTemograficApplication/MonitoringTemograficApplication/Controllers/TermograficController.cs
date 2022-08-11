729a5ec5f2c5a3a236719975ed8fa9a75e0850a5
Paulo Cergio de Almeida Costa Junior <paulo.junior@memt.com.br>
2022-08-11 15:46:52 -0300

    Atualização de segunda

--- MonitoringTemograficApplication/MonitoringTemograficApplication/Controllers/TermograficController.cs
+++ MonitoringTemograficApplication/MonitoringTemograficApplication/Controllers/TermograficController.cs
@@ -14,127 +14,129 @@ using System.Threading.Tasks;
 
 namespace MonitoringTemograficApplication.Controllers
 {
-  
-  public class TermograficController : Controller
-  {
-    private LogingClient _LoginClient;
-    private IUserRepository _userRepository;
-    private IProcessor _processor;
-    public TermograficController(LogingClient LoginClient, IUserRepository Userrepository , IProcessor processor)
-    {
-      _LoginClient = LoginClient;
-      _userRepository = Userrepository;
-      _processor = processor;
-    }
 
-    public IActionResult Index()
+    public class TermograficController : Controller
     {
-      return View();
-    }
-
-    [ClientAuthorizationAttibute]
-    public IActionResult ProcessorTermografic()
-    {
-      return View();
-    }
-    [HttpPost]
-    public IActionResult ProcessorTermografic([FromForm] Processor client)
-    {
-      
-      if (ModelState.IsValid)
-      {
-        var result = _processor.processorChange(client);
-        if(result != false)
+        private LogingClient _LoginClient;
+        private IUserRepository _userRepository;
+        private IProcessor _processor;
+        public TermograficController(LogingClient LoginClient, IUserRepository Userrepository, IProcessor processor)
         {
-          return RedirectToAction("Index", "Processor");
-        }
-        else
-        {
-          ViewData["MSG_A"] = "Algo deu errado ao processar";
-          return View();
+            _LoginClient = LoginClient;
+            _userRepository = Userrepository;
+            _processor = processor;
         }
 
-
-
-      }
-      else
-      {
-        StringBuilder sb = new StringBuilder();
-        foreach (var text in ModelState)
+        public IActionResult Index()
         {
-          foreach (var err in text.Value.Errors)
-          {
-            sb.Append(err.ErrorMessage + "<br />");
-          }
+            return View();
+        }
+
+        [ClientAuthorizationAttibute]
+        public IActionResult ProcessorTermografic()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ProcessorTermografic([FromForm] Processor client)
+        {
+
+
+
+            if (ModelState.IsValid)
+            {
+                var result = _processor.processorChange(client);
+                if (result != false)
+                {
+                    return RedirectToAction("Index", "Processor");
+                }
+                else
+                {
+                    ViewData["MSG_A"] = "Algo deu errado ao processar";
+                    return View();
+                }
+
+
+
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var text in ModelState)
+                {
+                    foreach (var err in text.Value.Errors)
+                    {
+                        sb.Append(err.ErrorMessage + "<br />");
+                    }
+
+                }
+                ViewData["MSG_E"] = sb.ToString();
+
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cancel()
+        {
+            return RedirectToAction("index", "Processor");
+        }
+
+        [ClientAuthorizationAttibute]
+        public IActionResult Privacy()
+        {
+            return View();
 
         }
-        ViewData["MSG_E"] = sb.ToString();
 
-      }
-      return View();
-    }
-    [HttpPost]
-    public IActionResult Cancel()
-    {
-      return RedirectToAction("Login", "Processor");
-    }
-
-    [ClientAuthorizationAttibute]
-    public IActionResult Privacy()
-    {
-      return View();
-
-    }
-    [HttpPost]
-    public IActionResult Logout()
-    {
-      _LoginClient.Logout();
-      return RedirectToAction("Login", "Termografic");
-    }
-
-    [HttpGet]
-    public IActionResult Login()
-    {
-      return View();
-    }
-
-    [HttpPost]
-    public IActionResult Login([FromForm] User client)
-    {
-      if (ModelState.IsValid)
-      {
-        bool userDB = _userRepository.Login(client.Usuario, client.Senha);
-
-        if (userDB != false)
+        public IActionResult Logout()
         {
-          _LoginClient.Login(client);
-
-          return RedirectToAction("Index", "Processor");
+            _LoginClient.Logout();
+            return RedirectToAction("Login", "Termografic");
         }
-        else
+
+        [HttpGet]
+        public IActionResult Login()
         {
-          ViewData["MSG_E"] = "Usuário não encontrado, verifique o usuário e senha digitado!";
-          return View();
+            return View();
         }
-      }
-      else
-      {
-        StringBuilder sb = new StringBuilder();
-        foreach (var text in ModelState)
+
+        [HttpPost]
+        public IActionResult Login([FromForm] User client)
         {
-          foreach (var err in text.Value.Errors)
-          {
-            sb.Append(err.ErrorMessage );
-          }
+            if (ModelState.IsValid)
+            {
+                bool userDB = _userRepository.Login(client.Usuario, client.Senha);
 
+                if (userDB != false)
+                {
+                    _LoginClient.Login(client);
+
+                    return RedirectToAction("Index", "Processor");
+                }
+                else
+                {
+                    ViewData["MSG_E"] = "Usuário não encontrado, verifique o usuário e senha digitado!";
+                    return View();
+                }
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var text in ModelState)
+                {
+                    foreach (var err in text.Value.Errors)
+                    {
+                        sb.Append(err.ErrorMessage);
+                    }
+
+                }
+                ViewData["MSG_E"] = sb.ToString();
+            }
+
+
+            var model = ModelState;
+            return View();
         }
-        ViewData["MSG_E"] = sb.ToString();
-      }
 
-
-      var model = ModelState;
-      return View();
     }
-
-  }
 }
--- MonitoringTemograficApplication/MonitoringTemograficApplication/Models/Processor.cs
+++ MonitoringTemograficApplication/MonitoringTemograficApplication/Models/Processor.cs
@@ -9,11 +9,12 @@ namespace MonitoringTemograficApplication.Models
 {
     public class Processor
     {
-        [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]        
+        [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+        [StringLength(10, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
         public string Corrida { get; set; }
 
         [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
-        
+        [StringLength(5, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
         public string Panela { get; set; }
     }
 }
