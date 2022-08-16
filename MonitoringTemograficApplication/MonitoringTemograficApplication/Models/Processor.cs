47fccb5e72249a79a7b71d7ea8bfab6ae4bdeb30
CarlosNunesMent <carlos.nunes@ment.com.br>
2022-08-16 14:02:06 -0300

    add atributte vida panela

--- MonitoringTemograficApplication/MonitoringTemograficApplication/Models/Processor.cs
+++ MonitoringTemograficApplication/MonitoringTemograficApplication/Models/Processor.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 
 namespace MonitoringTemograficApplication.Models
 {
-    public class Processor
-    {
-        [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
-        [StringLength(10, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
-        public string Corrida { get; set; }
+  public class Processor
+  {
+    [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+    [StringLength(10, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+    public string Corrida { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
-        [StringLength(5, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
-        public string Panela { get; set; }
-    }
+    [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+    [StringLength(5, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+    public string Panela { get; set; }
+
+    [Required(ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+    [StringLength(5, ErrorMessageResourceType = typeof(Menssage), ErrorMessageResourceName = "MSG_E001")]
+    public string VidaPanela { get; set; }
+
+
+  }
 }
